f494a520e7ed0dfb8c10af4053f8edb56bce3df9
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-06-01 19:06:35 +0530

    [Priyanshu] added new emp details

--- EmpRestSharpMSTest/UnitTest1.cs
+++ EmpRestSharpMSTest/UnitTest1.cs
@@ -47,7 +47,31 @@ namespace EmpRestSharpMSTest
                 Console.WriteLine("Id: " + emp.Id + "\t" + "Name: " + emp.Name + "\t" + "Salary: " + emp.Salary);
             }
         }
-        
+
+        //UC2 add new emp details
+        [TestMethod]
+        public void OnCallingPostAPI_ReturnEmployeeObject()
+        {
+            //Arrange
+            ///Initialize the request for POST to add new employee
+            RestRequest request = new RestRequest("Employees", Method.POST);
+            JsonObject jsonObj = new JsonObject();
+            jsonObj.Add("name", "Ritik");
+            jsonObj.Add("salary", "50000");
+            jsonObj.Add("id", "6");
+            ///Added parameters to the request object such as the content-type and attaching the jsonObj with the request
+            request.AddParameter("application/json", jsonObj, ParameterType.RequestBody);
+
+            //Act
+            IRestResponse response = client.Execute(request);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            Employee employee = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Ritik", employee.Name);
+            Assert.AreEqual("50000", employee.Salary);
+            Console.WriteLine(response.Content);
+        }
     }
 }
 
