540806f44c6bf2124294da53e8f3406b0f420348
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-06-01 19:04:14 +0530

    [Priyanshu] added retrieval using json server

--- EmpRestSharpMSTest/UnitTest1.cs
+++ EmpRestSharpMSTest/UnitTest1.cs
@@ -31,9 +31,8 @@ namespace EmpRestSharpMSTest
             return response;
         }
 
-        /// <summary>
-        /// UC1 Retrieve all employee details in the json file
-        /// </summary>
+        
+        // UC1 Retrieve all employee details in the json file 
         [TestMethod]
         public void OnCallingGetAPI_ReturnEmployeeList()
         {
@@ -48,29 +47,7 @@ namespace EmpRestSharpMSTest
                 Console.WriteLine("Id: " + emp.Id + "\t" + "Name: " + emp.Name + "\t" + "Salary: " + emp.Salary);
             }
         }
-        [TestMethod]
-        public void OnCallingPostAPI_ReturnEmployeeObject()
-        {
-            //Arrange
-            ///Initialize the request for POST to add new employee
-            RestRequest request = new RestRequest("Employees", Method.POST);
-            JsonObject jsonObj = new JsonObject();
-            jsonObj.Add("name", "Ritik");
-            jsonObj.Add("salary", "50000");
-            jsonObj.Add("id", "6");
-            ///Added parameters to the request object such as the content-type and attaching the jsonObj with the request
-            request.AddParameter("application/json", jsonObj, ParameterType.RequestBody);
-
-            //Act
-            IRestResponse response = client.Execute(request);
-
-            //Assert
-            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
-            Employee employee = JsonConvert.DeserializeObject<Employee>(response.Content);
-            Assert.AreEqual("Ritik", employee.Name);
-            Assert.AreEqual("50000", employee.Salary);
-            Console.WriteLine(response.Content);
-        }
+        
     }
 }
 
